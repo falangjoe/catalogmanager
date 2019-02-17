15836a6da7a234291a8735fdc6c343a04da92b91
Joseph Smith <jsmith3@1800contacts.com>
2019-02-17 16:54:07 -0700

    trying to make reusable form component logic

---
+++ src/app/helpers/form.helpers.ts
@@ -0,0 +1,82 @@
+import {Provider,  forwardRef, ForwardRefFn  } from '@angular/core';
+import {ControlValueAccessor, NG_VALUE_ACCESSOR, NG_VALIDATORS, AbstractControl, Validator, ValidationErrors } from '@angular/forms';
+
+export function DefaultControlAccessorProvider(forwardRefFn: ForwardRefFn) : Provider{
+
+    return  {
+        provide: NG_VALUE_ACCESSOR,
+        useExisting: forwardRef(forwardRefFn),
+        multi: true
+      };
+}
+
+
+
+export function DefaultControlValidatorProvider(forwardRefFn: ForwardRefFn) : Provider{
+
+    return  {
+        provide:  NG_VALIDATORS,
+        useExisting: forwardRef(forwardRefFn),
+        multi: true
+      };
+}
+
+
+export class DefaultControlValueAccessor implements ControlValueAccessor, Validator{
+
+
+    constructor(private abstractControl: AbstractControl) {
+
+        this._control = abstractControl;
+
+    }
+
+    public registerOnValidatorChange?(fn: () => void): void {
+      
+    }
+
+    public validate(control: AbstractControl): ValidationErrors | null{
+
+        return this._control.invalid ? {err : "invalid"} : null;
+    }
+
+    private _control : AbstractControl;
+
+    public writeValue(obj: any): void {
+        if(obj){
+          this._control.setValue(obj, {emitEvent : false});
+        }
+    
+      }
+    
+    public registerOnChange(fn: any): void {
+        this._control.valueChanges.subscribe(fn);
+    }
+    
+    public registerOnTouched(fn: any): void {
+    
+    }
+    
+    public setDisabledState?(isDisabled: boolean): void {
+        if(isDisabled && this._control.enabled){
+          this._control.disable({emitEvent : false});
+        }else if(!isDisabled && this._control.disable){
+          this._control.enable({emitEvent : false});
+        }
+    }
+
+    // public validate(c : AbstractControl) {
+    //     // this.control.get('environment').markAsTouched({ onlySelf: true });
+    //     // this.control.get('catalog').markAsTouched({ onlySelf: true });  
+    //     return this._control.invalid ? {err : "Form Group Invalid"} : null;
+    // }
+
+    public get control() {
+        return this._control;
+    }
+
+  
+
+
+
+}
--- src/app/intervals/cm-catalog/cm-catalog.component.ts
+++ src/app/intervals/cm-catalog/cm-catalog.component.ts
@@ -1,76 +1,62 @@
-import {Component, OnInit,  forwardRef  } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {CatalogService } from '../catalog.service';
-import {FormGroup, FormControl, Validators, ControlValueAccessor, NG_VALUE_ACCESSOR, NG_VALIDATORS } from '@angular/forms';
+import {FormGroup, FormControl, Validators, AbstractControl} from '@angular/forms';
+import {DefaultControlAccessorProvider,DefaultControlValidatorProvider,DefaultControlValueAccessor} from '../../helpers/form.helpers';
 
 @Component({
   selector: 'cm-catalog',
   templateUrl: './cm-catalog.component.html',
   styleUrls: ['./cm-catalog.component.css'],
-  providers: [
-    {
-      provide: NG_VALUE_ACCESSOR,
-      useExisting: forwardRef(() => CmCatalogComponent),
-      multi: true
-    },  
-    { provide: NG_VALIDATORS, 
-      useExisting: forwardRef(() => CmCatalogComponent), 
-      multi: true 
-    }
+  providers : [
+    DefaultControlAccessorProvider(() => CmCatalogComponent),
+    DefaultControlValidatorProvider(() => CmCatalogComponent),
   ]
 })
-export class CmCatalogComponent implements OnInit , ControlValueAccessor {
-
-  writeValue(obj: any): void {
-    if(obj){
-      this.control.setValue(obj, {emitEvent : false});
-    }
-
-  }
-
-  registerOnChange(fn: any): void {
-    this.control.valueChanges.subscribe(fn);
-  }
-
-  registerOnTouched(fn: any): void {
-
-  }
-
-  setDisabledState?(isDisabled: boolean): void {
-    if(isDisabled && this.control.enabled){
-      this.control.disable({emitEvent : false});
-    }else if(!isDisabled && this.control.disable){
-      this.control.enable({emitEvent : false});
-    }
-  }
+export class CmCatalogComponent extends DefaultControlValueAccessor implements OnInit  {
 
   constructor(catalogService : CatalogService) { 
 
+    super(new FormGroup({
+      environment: new FormControl('',Validators.required),
+      catalog: new FormControl('',Validators.required),
+    }));
+
     this.catalogService = catalogService;
+
+    this.environment = this.control.get('environment');
+    this.catalog = this.control.get('catalog');
   }
 
-  private catalogService;
+  private catalogService : CatalogService;
+
+  private environment : AbstractControl;
+  private catalog : AbstractControl;
+
+  private catalogs : Array<string> = [];
+  private environments : Array<string> = [];
 
   ngOnInit() {
 
+    this.setEnvironments();
+    this.subscribeEnvironmentValueChanges();
+  }
+
+  private setEnvironments() : void{
     this.catalogService.getEnvironments().subscribe(x => {
       this.environments = x;
     });
+  }
 
-    this.control.get('environment').valueChanges.subscribe(x => {
-      this.control.patchValue({catalog : undefined}, {emitEvent: false});
+  private subscribeEnvironmentValueChanges() : void{
+
+    this.environment.valueChanges.subscribe(x => {
+      this.catalog.setValue(undefined);
       this.setCatalogs(x);
 
     });
-
-    // this.control.get('environment').markAsTouched({ onlySelf: true });
-    // this.control.get('catalog').markAsTouched({ onlySelf: true });  
-
   }
 
-  private catalogs = [];
-  private environments = [];
-
-  private setCatalogs(environment){
+  private setCatalogs(environment) : void{
     if(environment){
       this.catalogService.getCatalogs(environment).subscribe(x => {
         this.catalogs = x;
@@ -82,16 +68,4 @@ export class CmCatalogComponent implements OnInit , ControlValueAccessor {
     }
   }
 
-  public control = new FormGroup({
-    environment: new FormControl('',Validators.required),
-    catalog: new FormControl('',Validators.required),
-  });
-
-  validate(c: FormControl) {
-    return this.control.invalid ? {catalog : {}}: null;
-  }
-
-
-  
-  
 }
